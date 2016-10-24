a2ff2762d5409de1bdef543d447144304a9b4450
Ard van Breemen <ard@kwaak.net>
2016-10-24 16:59:27 +0200

    We don't get, we just git

--- get-gst.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash -x
-. ./set_env.sh
-wget -c https://gstreamer.freedesktop.org/src/gstreamer/gstreamer-$VER.tar.xz
-wget -c https://gstreamer.freedesktop.org/src/gstreamer/gstreamer-$VER.tar.xz.sha256sum
-wget -c https://gstreamer.freedesktop.org/src/gst-plugins-base/gst-plugins-base-$VER.tar.xz
-wget -c https://gstreamer.freedesktop.org/src/gst-plugins-base/gst-plugins-base-$VER.tar.xz.sha256sum
