2b810c65aa8b20961c74d8b053d4046d00014061
Ard van Breemen <ard@kwaak.net>
2016-10-26 13:56:50 +0200

    commit for test on armhf

---
+++ do_gst-plugins-ugly.sh
@@ -0,0 +1,10 @@
+#!/bin/bash -x
+. ./set_env.sh
+mkdir -p $PFX
+cd gst-plugins-ugly
+git clean -x -d -f
+git checkout 1.9.90
+export CONFIGURE_EXT_OPT="--prefix=$PFX --enable-introspection=yes --enable-v4l2-probe"
+./autogen.sh
+make
+make install
