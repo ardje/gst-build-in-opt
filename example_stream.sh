f491cfb5025e2a11ef16e2002df6e09014092528
Odroid @ odroid9 <ard@kwaak.net>
2016-11-03 09:48:28 +0000

    tsrtp2 from the g+ post

--- example_stream.sh
+++ example_stream.sh
@@ -31,11 +31,24 @@ rtp() {
 	GSL v4l2src !  video/x-raw, format=$FORMAT,framerate=25/1,width=1280,height=720 ! videoconvert !  x264enc ! h264parse ! rtph264pay config-interval=10 !  udpsink host=$DMCAST port=$DPORT auto-multicast=true
 }
 tsrtp() {
-	GSL v4l2src !  video/x-raw, format=$FORMAT,framerate=25/1,width=1280,height=720 ! videoconvert !  x264enc ! mpegtsmux ! rtpmp2tpay !  udpsink host=$DMCAST port=$DPORT auto-multicast=true
+	GSL v4l2src !  video/x-raw, format=$FORMAT,framerate=25/1, width=1280, height=720 ! videoconvert !  x264enc ! mpegtsmux ! rtpmp2tpay !  udpsink host=$DMCAST port=$DPORT auto-multicast=true
+}
+tsrtp2() {
+	GSL \
+	v4l2src ! video/x-raw, format=$FORMAT,framerate=25/1, width=1280, height=720 ! videoconvert ! queue ! x264enc speed-preset=1 ! h264parse ! mpegtsmux ! rtpmp2tpay '!' udpsink host=239.255.12.42 port=5004 auto-multicast=true
 }
 # example for working tsrtp of mp4 encoded files
 tsrtpvlc() { 
 	cvlc --loop "$@" --sout '#rtp{mux=ts,caching=2000,dst='"${DMCAST}"',port='"${DPORT}"',sdp=sap://,name="meeh",dts-delay=1000}'
+}
+tsrtpfile() {
+	GSL filesrc location="$1" ! qtdemux name=demuxed \
+		mpegtsmux name=tsmux \
+		queue name=quadio \
+		queue name=qvideo \
+		demuxed.audio_00 ! qaudio. ! tsmux. \
+		demuxed.video_00 ! qvideo. ! tsmux. \
+		tsmux. ! rtpmp2tpay !  udpsink host=$DMCAST port=$DPORT auto-multicast=true
 }
 rtpplay() {
 	# Does not work with this build, works with standard gstreamer
