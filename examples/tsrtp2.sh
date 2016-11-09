d513bf0637fcf3bb39d29572ff9dade3da72a0cb
Odroid @ odroid9 <ard@kwaak.net>
2016-11-09 13:53:55 +0000

    Still not getting right latency

--- examples/tsrtp2.sh
+++ examples/tsrtp2.sh
@@ -8,12 +8,12 @@ tsrtp2() {
         GSL \
 	v4l2src do-timestamp=true ! \
 	video/x-raw, format=$FORMAT,framerate=25/1, width=1280, height=720 ! \
-	queue max-size-bytes=335544320 min-threshold-time=20000000 leaky=0 ! \
+	queue max-size-bytes=335544320 min-threshold-time=40000000 leaky=0 ! \
 	videoconvert ! \
 	x264enc rc-lookahead=80 tune=0 threads=6 psy-tune=0 speed-preset=3  ! \
-	queue min-threshold-time=10000000 ! \
 	h264parse ! \
 	mpegtsmux ! \
 	rtpmp2tpay ! \
-	udpsink host=239.255.12.42 port=5004 auto-multicast=true sync=true
+	queue max-size-bytes=335544320 min-threshold-buffers=50 ! \
+	udpsink host=239.255.12.42 port=5004 auto-multicast=true  sync=true
 }
