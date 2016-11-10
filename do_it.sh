1708c46e649bf7eb15b1589a3e734bce4c646183
Odroid @ odroid9 <ard@kwaak.net>
2016-11-10 11:29:10 +0000

    Do it all..

--- do_it.sh
+++ do_it.sh
@@ -7,5 +7,12 @@ fi
 
 ./do_checkconfig.sh
 ./do_orc.sh
-wget -c https://www.khronos.org/registry/khronos_headers.tgz
-tar xvzf khronos_headers.tgz -C $PFX/include/ --strip-components=1
+#wget -c https://www.khronos.org/registry/khronos_headers.tgz
+#tar xvzf khronos_headers.tgz -C $PFX/include/ --strip-components=1
+./do_x264.sh
+./do_gstreamer.sh
+./do_gst-plugins-base.sh
+./do_gst-plugins-good.sh
+./do_gst-plugins-bad.sh
+./do_gst-plugins-ugly.sh
+
