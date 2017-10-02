7cdfe262a1bf180c4f03a5b0a859ef2c0291bf06
Root DC0 kwaak <root@u3>
2017-10-02 05:45:35 +0200

    Upgrade to orc 0.4.27

--- do_orc.sh
+++ do_orc.sh
@@ -3,7 +3,7 @@
 mkdir -p $PFX
 cd orc
 git clean -x -d -f
-git checkout orc-0.4.26
+git checkout orc-0.4.27
 export CONFIGURE_EXT_OPT="--prefix=$PFX --enable-introspection=yes"
 NOCONFIGURE=true ./autogen.sh
 ./configure $CONFIGURE_EXT_OPT
